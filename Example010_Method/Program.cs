4083e2eae11e897eb7edc9c338ba9ef80eaad70b
korvyn <korvyn@yandex.ru>
2022-10-10 15:56:05 +0300

    Ex010

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {11, 19, 21, 3111, 1401, 152, 61, 17, 18, 19};
+
+int n = array.Length;
+
+int find = 19;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
